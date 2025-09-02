b3dc7435c8dc4b68f77e29e264f4eaca3fc035af
Logan Archambault Vallee <6311993@cstjean.qc.ca>
2025-09-02 15:39:28 -0400

    2e commit

--- src/cstjean/mobile/travail/Pion.java
+++ src/cstjean/mobile/travail/Pion.java
@@ -3,8 +3,14 @@ package cstjean.mobile.travail;
 public class Pion {
     private final String couleur;
 
-    public Pion(String couleur) {
+    public Pion(String couleur)
+    {
         this.couleur = couleur;
+    }
+
+    public Pion()
+    {
+        couleur = "Blanc";
     }
 
     public String GetCouleur() {
--- tests/cstjean/mobile/travail/TestPions.java
+++ tests/cstjean/mobile/travail/TestPions.java
@@ -4,15 +4,20 @@ import junit.framework.TestCase;
 public class TestPions extends TestCase{
 
     public void testCouleur() {
-        Pion pion = new Pion("Rouge");
-        assertEquals("Rouge", pion.GetCouleur());
+        Pion pion = new Pion("Blanc");
+        assertEquals("Blanc", pion.GetCouleur());
 
         Pion pion2 = new Pion("Noir");
         assertEquals("Noir", pion2.GetCouleur());
     }
     public void testCreer() {
-        Pion pion1 = new Pion("Rouge");
-        Damier damier = new Damier();
+        String couleur = "Blanc";
+        Pion pion1 = new Pion(couleur);
+        assertEquals(couleur, pion1.GetCouleur());
+
+
+        Pion pion2 = new Pion();
+        assertEquals(couleur, pion2.GetCouleur());
     }
 
 }
