488c144dff39642e77ce8eb9ebe7fc49ae8f0897
Logan Archambault Vallee <6311993@cstjean.qc.ca>
2025-09-05 11:57:20 -0400

    commentaire

--- src/cstjean/mobile/travail/Damier.java
+++ src/cstjean/mobile/travail/Damier.java
@@ -3,14 +3,30 @@ package cstjean.mobile.travail;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Représente un damier contenant une liste de pions.
+ */
 public class Damier {
 
+    /** Liste des pions placés sur le damier. */
     private final List<Pion> damier;
+
+    /**
+     * Constructeur par défaut.
+     * Initialise une liste vide de pions.
+     */
     public Damier() {
         this.damier = new ArrayList<>();
     }
 
+    /**
+     * Ajoute un pion à une position spécifique du damier.
+     *
+     * @param index l'index où insérer le pion
+     * @param pion le pion à ajouter
+     * @throws IndexOutOfBoundsException si l'index est invalide
+     */
     public void ajouterPion(int index, Pion pion) {
-        pion.add(index);
+        damier.add(index, pion);
     }
 }
